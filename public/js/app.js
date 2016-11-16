967d882a52900ad7b6e67a411cf2a19f66f42270
Zarela Velasquez <zarelavp@yahoo.com>
2016-11-15 23:58:05 -0500

    Created landing page and router file

---
+++ public/js/app.js
@@ -0,0 +1,24 @@
+(function() {
+  angular
+  .module('concert', ['ui.router'])
+  .config(MainRouter);
+
+  MainRouter.$inject = ['$stateProvider', '$urlRouterProvider', '$locationProvider'];
+
+  function MainRouter($stateProvider, $urlRouterProvider, $locationProvider) {
+    $stateProvider
+    .state('home', {
+      url:'/'
+      templateUrl: 'home.html'
+    })
+
+    $urlRouterProvider.otherwise('/');
+
+    $locationProvider.html5Mode({
+      enabled: true,
+      requireBase: false
+    });
+
+  } //end
+})()
+console.log("Controller wired")
--- server.js
+++ server.js
@@ -1,16 +1,25 @@
+//MODULES
+//=============================================
 var express = require('express');
 var app     = express();
 var path    = require('path');
 
 app.use(express.static(path.join(__dirname, 'public')));
 
-//Routing
+//ROUTING
+//============================================
 app.get('/', function(req, res){
-  res.send("Hello World");
-  // res.render('index');
+  // res.send("Hello World");
+  res.render('index');
 });
 
-//Port listener
+//Allows $locationProvider to work without #
+app.all('/*', function(req, res, next) {
+  res.sendFile('/public/index.html', { root: __dirname });
+});
+
+//PORT LISTENER
+//=============================================
 app.listen(process.env.PORT || 4000, function(){
   console.log("=============================");
   console.log("SERVER LISTENING ON PORT 4000");
