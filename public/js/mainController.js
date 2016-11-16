dc0fb1894548c6f30ebb5a8ad2cafa0c36a9af5a
Zarela Velasquez <zarelavp@yahoo.com>
2016-11-16 13:39:07 -0500

    Registration working and saving to database

--- public/js/mainController.js
+++ public/js/mainController.js
@@ -6,6 +6,66 @@
     var self = this;
     var rootUrl = "http://localhost:3000";
 
-  }
+    this.signup = function(user) {
+      console.log(user);
+      self.signed = user;
+      return $http({
+        url:  `${rootUrl}/users`,
+        method: 'POST',
+        data: {user:user}
+      })
+      .then(function(response) {
+        console.log(response);
+        if (response.data.status === 200) {
+          console.log('User registration successful');
+          self.sucess = true;
+          self.login(self.signed);
+        }
+        else {
+          console.log(response)
+          if (response.data.user.email) {
+            failAlert('Registration failed. Email ' + response.data.user.email[0]);
+          } else if (response.data.user.username) {
+            failAlert('Registration failed '+ response.data.user.username[0]);
+          }
+        }
 
+      })
+      .catch(function(err) {
+        console.log(err);
+      })
+    }
+
+  // this.login = function(user) {
+  //   return $http ({
+  //     url: `${rootUrl}/users/login`,
+  //     method: 'POST',
+  //     data: {user: user}
+  //   })
+  //   .then(function(response){
+  //     if (response.data.status == 401) {
+  //       failAlert('Unauthorized! Check your username and password!')
+  //       self.user.password = '',
+  //     }
+  //     console.log(response);
+  //     passAlert('<strong>Success!</strong> Hi there, ' + response.data.user.username + '.')
+  //     self.user = response.data.user;
+  //     self.id = response.data.user.id;
+  //     console.log('Token ~>', response.data.token);
+  //     localStorage.setItem('token', JSON.stringify(response.data.token))
+  //     $state.go('profile', {url:'/profile', user: response.data.user})
+  //   })
+  //   .catch(function(err) {
+  //     console.log(err);
+  //   })
+  // }
+
+  $http.get(`${rootUrl}/users`)
+  .then(function(response) {
+    console.log(response);
+    self.users = response.data.users
+  })
+
+
+  } //end
 })()
